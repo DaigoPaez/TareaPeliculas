d2873fc6b400e11c33dc997bed00805120ecf05c
DaigoPaez <126517336+DaigoPaez@users.noreply.github.com>
2023-03-22 00:53:46 +0000

    Mas o menos así?

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -18,7 +18,16 @@ namespace Pelicula
         public Pelicula(string titulo, int año){}
 
         //Métodos
-     
+        public string GetTitulo(){return titulo;}
+        public void SetTitulo(string title){title=titulo;}
+        public int GetAño(){return año;}
+        public void SetAño(int year){year=año;}
+        public string GetPais(){return pais;}
+        public string GetDirector(){return director;}
+        public void AgregaActor(Actor actor){actores.Add(actor);}
+        public void ImprimeActores()
+        {
+        }
         public void Imprime()
         {
             Console.WriteLine($"{titulo} ({año})");
@@ -32,11 +41,15 @@ namespace Pelicula
     public class Actor
     {
         //Propiedades
-
+        string Nombre;
+        int Año;
         //Constructores
-
+        public Actor(){}
+        public Actor(string Nombre, int Año){}
 
         //Métodos 
+        public string GetNombre(){return Nombre;}
+        public int GetAño(){return Año;}
         public void Imprime()
         {
             Console.WriteLine($"{Nombre} ({Año})");
@@ -59,11 +72,16 @@ namespace Pelicula
             Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño());
 
             List<Pelicula> peliculas = new List<Pelicula>();
-            peliculas.Add(new Pelicula ("1"));
-            peliculas.Add(new Pelicula ("2"));
-            peliculas.Add(new Pelicula ("3"));
-            peliculas.Add(new Pelicula ("4"));
-            peliculas.Add(new Pelicula ("5"));
+            peliculas.Add(new Pelicula ("1", 2020));
+            peliculas.Add(new Pelicula ("2", 2012));
+            peliculas.Add(new Pelicula ("3", 2005));
+            peliculas.Add(new Pelicula ("4", 1986));
+            peliculas.Add(new Pelicula ("5", 2013));
+
+            p1.AgregaActor(new Actor("Ryan Gosling", 1980));
+            p1.AgregaActor(new Actor("Emma Stone", 1988));
+
+            p1.ImprimeActores();
         }
     }
 }
